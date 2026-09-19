d4095bc9b2d8f474beebff8f2da9c0c8ba779a6d
kartikkamble481 <kartikkamble481@gmail.com>
2026-09-19 19:52:59 +0530

    added index.html, style.css, script.js file in arrays folder and arrays in js

---
+++ script.js
@@ -0,0 +1,47 @@
+//  Arrays in js 
+
+
+console.log("Arrays in js");
+
+let heroes = ["spiderman", "ironman", "thor", "hulk"];
+let villen = [, "thenos", "kalki","elian"];
+
+let mix = heroes + villen ;
+console.log(mix);
+console.log(heroes);
+console.log(heroes.length);
+for(hero of heroes){
+    console.log(hero);
+}
+
+
+// Arrays indices in arrays 
+
+console.log("arrays indices")
+;
+let marks = [ 90, 95, 80, 85, 70, 75];
+
+marks[0]= 99, marks[1]= 98, marks[2]= 97, marks[3]= 96, marks[4]= 95, marks[5]= 94, marks[6]= 93, marks[7]= 92, marks[8]= 91;
+
+
+console.log(marks);
+
+// for (let i = 0; i < heroes.length; i++){
+//     console.log(i);
+//  }
+
+for(let mark of marks){
+    console.log(mark);
+}
+
+
+
+let citys = ["mumbai", "pune", "sangli", "kolhapur", "delhi", "goa"];
+
+citys [1]= "newari";
+
+
+for(city of citys){
+    console.log ("city =", city.length, city);
+}
+
